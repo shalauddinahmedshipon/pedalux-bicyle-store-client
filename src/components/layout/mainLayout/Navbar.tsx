7bcfd300dc81084cc24b98b28527ddd70d380017
MD. SHIPON <sshimi910@gmail.com>
2025-03-12 20:13:01 -0800

    responsive drawer design done for small device

--- src/components/layout/mainLayout/Navbar.tsx
+++ src/components/layout/mainLayout/Navbar.tsx
@@ -1,11 +1,14 @@
 import { Input } from "@/components/ui/input";
-import { CiSearch } from "react-icons/ci";
+import { CiSearch, CiUser } from "react-icons/ci";
 import "../../../styles/Navbar.css"
-import { IoCartOutline } from "react-icons/io5";
+import { IoCartOutline, IoMenuSharp } from "react-icons/io5";
 import { GrFavorite } from "react-icons/gr";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 
 const Navbar = () => {
+  const [openSidebar,setOpenSideBar] = useState<boolean>(false);
   return (
    
 <nav className="flex justify-between items-center  mx-8 my-1">
@@ -19,12 +22,13 @@ const Navbar = () => {
       
       {/* nav center*/}
      <div >
-  <ul className="flex gap-8">
+  <ul className="lg:flex gap-8 hidden">
     
-      <li className=" active-link">Home</li>
+      <li className="active-link">Home</li>
       <li className="nav-link">Products</li>
       <li className="nav-link">About</li>
       <li className="nav-link">Contact</li>
+    
     
   </ul>
      </div>
@@ -32,9 +36,13 @@ const Navbar = () => {
 
     {/* nav end  */}
     <div className="flex justify-between items-center gap-6">
-    <div className="relative">
+      {/* input field  lg*/}
+    <div className="relative hidden lg:block">
     <CiSearch className="absolute right-2 text-2xl top-1.5 text-gray-900"/>
     <Input type="text" placeholder="Search" className="pl-6"/>
+    </div>
+    <div className="block lg:hidden">
+    <CiSearch className=" text-3xl text-gray-900"/>
     </div>
     <div>
     <GrFavorite className="text-2xl " />
@@ -42,10 +50,46 @@ const Navbar = () => {
     <div>
     <IoCartOutline className="text-3xl" />
     </div>
-    <button className="text-rose-500 transition-colors duration-300 hover:bg-rose-500 hover:text-white border-rose-500 border-[1.5px] active:scale-95 px-6 py-1 font-medium rounded-full">
+    <button className="text-rose-500 transition-colors duration-300 hover:bg-rose-500 hover:text-white border-rose-500 border-[1.5px] active:scale-95 px-6 py-1 font-medium rounded-full hidden lg:block">
     Sign Up
     </button>
     </div>
+
+
+
+               {/* nav item for small device  */}
+<div onClick={()=>setOpenSideBar(!openSidebar)} className='block lg:hidden '>
+ {/* Overlay */}
+ <div className={`  ${openSidebar?'fixed inset-0 bg-black/10 bg-opacity-50 ':' '}`}></div>
+
+{
+  !openSidebar&& <IoMenuSharp onClick={()=>setOpenSideBar(!openSidebar)} className='text-3xl text-gray-600 relative z-10' />
+}
+
+  <div   className={`bg-white shadow-2xl fixed transition-all duration-1000 z-50 top-0   flex flex-col   py-2  w-[300px]   ${openSidebar?'right-0 ':'-right-[300px]'} h-screen`}>
+  
+
+   <div >
+  <ul className="pl-5">
+    
+      <li className="active-link-small py-2">Home</li>
+      <li className="nav-link-small py-2">Products</li>
+      <li className="nav-link-small py-2">About</li>
+      <li className="nav-link-small py-2">Contact</li>
+     
+    </ul>
+ 
+  <div className="pl-5 ">
+  <li className=" py-2 flex items-center justify-baseline border-b-[3px] mt-4 border-rose-500 font-semibold"><CiUser className="text-2xl"/> Sign Up</li>
+  </div>
+     </div>
+ 
+    
+  </div>
+
+
+</div>  
+
 </nav>
   
 
